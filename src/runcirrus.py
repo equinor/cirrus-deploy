9e56b34d937651fd29b80ee9e06c07679438cdab
Tenné <me@wah.pink>
2024-12-06 13:54:22 +0100

    runcirrus: Elaborate on usage in --help text

--- src/runcirrus.py
+++ src/runcirrus.py
@@ -1,4 +1,39 @@
 #!/usr/bin/python3.11
+"""Wrapper for running Cirrus with MPI in Equinor.
+
+This script operates on Cirrus .in files and enables you to simulate in
+parallell. For example, given the "spe1.in" case, you can simply run it with the
+following command:
+
+    $ runcirrus spe1.in
+
+This will use all available cores on your local machine, and output the
+following files:
+
+    'spe1.out': Text summarising the simulation
+    'spe1-mas.dat':
+    'spe1.INIT':
+    'spe1.SMSPEC':
+    'spe1.UNSMRY':
+
+Additionally, runcirrus produces the following files:
+
+    'spe1.LOG': Cirrus' "stdout" standard output
+    'spe1.ERR': Cirrus' "stderr" standard error
+    'spe1_bsub.LOG': Logs from the workflow manager when using IBM LSF
+
+To utilise the HPC cluster, specify '-q' (aka. '--queue'). In this
+configuration, only 1 CPU will be utilised by default. To change this behaviour,
+use the '-n' and '-m' options. '-n' is "number of tasks per machine" and '-m' is
+"number of machines".
+
+For example, to add a job to the 'bigmem' queue using 2 machines (nodes) and 8
+processes per machine for a total of 16 cores, use:
+
+    $ runcirrus -q bigmem -n 8 -m 2 spe1.in
+
+"""
+
 from __future__ import annotations
 import sys
 from typing import Any, NoReturn
@@ -117,7 +152,9 @@ class Arguments:
 
 def parse_args(argv: list[str]) -> Arguments:
     ap = argparse.ArgumentParser(
-        description="Wrapper for running Cirrus with MPI in Equinor"
+        prog="runcirrus",
+        formatter_class=argparse.RawDescriptionHelpFormatter,
+        epilog=__doc__,
     )
     ap.add_argument("input", help="Cirrus .in input file")
     ap.add_argument(
