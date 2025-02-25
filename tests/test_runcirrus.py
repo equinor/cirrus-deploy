5888bb75d72855c9591dd435d7a37dfcb7671068
Zohar Malamant <ZOM@equinor.com>
2025-02-25 16:33:13 +0100

    Fix runcirrus.py and asyncio tests

--- tests/test_runcirrus.py
+++ tests/test_runcirrus.py
@@ -1,10 +1,37 @@
 import os
+import sys
+import importlib.util
+from pathlib import Path
 
-from src import runcirrus
 import pytest
 
 
-def test_printversions_empty_folder(capsys, tmp_path, monkeypatch):
+@pytest.fixture(scope="session")
+def runcirrus():
+    """Python doesn't know where to look for runcirrus.py, so we can't simply
+    import it. Instead, and to avoid explicitly modifying `sys.path`, we
+    dynamically import the file under the `runcirrus` module name.
+
+    This seems less hacky.
+
+    """
+
+    file_location = (
+        Path(os.path.dirname(__file__)).parent / "src/deploy/scripts/runcirrus.py"
+    ).resolve()
+
+    spec = importlib.util.spec_from_file_location("runcirrus", file_location)
+    assert spec is not None
+    assert spec.loader is not None
+
+    runcirrus = importlib.util.module_from_spec(spec)
+
+    sys.modules["runcirrus"] = runcirrus
+    spec.loader.exec_module(runcirrus)
+    return runcirrus
+
+
+def test_printversions_empty_folder(capsys, tmp_path, monkeypatch, runcirrus):
     monkeypatch.setenv("CIRRUS_VERSIONS_PATH", str(tmp_path))
     with pytest.raises(SystemExit):
         runcirrus.parse_args(["0", "--print-versions"])
@@ -15,7 +42,9 @@ def test_printversions_empty_folder(capsys, tmp_path, monkeypatch):
 @pytest.mark.parametrize(
     "dirs,expected_out", [(["1.10"], "1.10\n"), (["1.10", ".store"], "1.10\n")]
 )
-def test_printversions_ignore_hidden(dirs, expected_out, capsys, tmp_path, monkeypatch):
+def test_printversions_ignore_hidden(
+    dirs, expected_out, capsys, tmp_path, monkeypatch, runcirrus
+):
     for name in dirs:
         (tmp_path / name).mkdir()
     monkeypatch.setenv("CIRRUS_VERSIONS_PATH", str(tmp_path))
@@ -35,7 +64,7 @@ def test_printversions_ignore_hidden(dirs, expected_out, capsys, tmp_path, monke
         ("runpflotran1.8-openpbs-rh8", "1.8"),
     ],
 )
-def test_default_version(script_name, expect):
+def test_default_version(script_name, expect, runcirrus):
     assert runcirrus.default_version(script_name) == expect, f"{script_name=}"
 
 
@@ -50,7 +79,7 @@ def test_default_version(script_name, expect):
     ],
 )
 def test_get_versions_path_correctly_identifies_location(
-    dir, expected_out, tmp_path, monkeypatch
+    dir, expected_out, tmp_path, monkeypatch, runcirrus
 ):
     def mockreturn(_):
         return dir
@@ -61,7 +90,7 @@ def test_get_versions_path_correctly_identifies_location(
 
 
 def test_get_versions_path_correctly_identifies_symlinked_location(
-    tmp_path, monkeypatch
+    tmp_path, monkeypatch, runcirrus
 ):
     script_path = (
         tmp_path / "cirrus/versions/.store/d312321-runcirrus-1.0.0/bin/runcirrus"
@@ -79,7 +108,7 @@ def test_get_versions_path_correctly_identifies_symlinked_location(
     assert str(runcirrus.get_versions_path()).endswith("")
 
 
-def test_get_versions_path_correctly_fails(capsys, monkeypatch):
+def test_get_versions_path_correctly_fails(capsys, monkeypatch, runcirrus):
     def mockreturn(_):
         # the important thing is that "versions" does not exist in path
         return "/some/example/path"
