739021588ac06f274b62bfd824673599c8f70026
Zohar Malamant <ZOM@equinor.com>
2025-03-06 16:17:12 +0100

    Add pretty print to check

--- src/deploy/check.py
+++ src/deploy/check.py
@@ -2,11 +2,18 @@ from __future__ import annotations
 import sys
 import asyncio
 from pathlib import Path
+from typing import Awaitable, Coroutine, Iterable, TypeVar
 
 import yaml
 from pydantic import BaseModel
 
 from deploy.config import Config, AreaConfig
+from rich.console import Console
+from rich.progress import Progress
+from rich.table import Table
+
+
+console = Console()
 
 
 class Collect(BaseModel):
@@ -37,42 +44,56 @@ async def collect(config: Config, area: AreaConfig) -> Collect:
     )
     stdout, stderr = await proc.communicate(SCRIPT)
 
-    print(f"Finished area {area.name}")
+    console.log(f"Finished area {area.name}")
 
     return Collect.model_validate(yaml.safe_load(stdout))
 
 
-async def _check(config: Config) -> None:
-    tasks: list[asyncio.Task[Collect]] = []
+_T = TypeVar("_T")
 
+
+async def _pretty_gather(*coros: Awaitable[_T]) -> list[_T]:
+    tasks: list[asyncio.Task[_T]] = []
+    with Progress(console=console) as progress:
+        progress_task = progress.add_task("[cyan]Checking...", total=len(coros))
+
+        for coro in coros:
+            task = asyncio.create_task(coro)
+            task.add_done_callback(lambda *_: progress.advance(progress_task))
+            tasks.append(task)
+
+        return await asyncio.gather(*tasks)
+
+
+async def _check(config: Config) -> None:
     if not config.areas:
         sys.exit("No areas specified in config.yaml")
 
-    for area in config.areas:
-        task = asyncio.create_task(collect(config, area))
-        tasks.append(task)
-
-    collected: dict[str, Collect] = {}
-
-    for area, info in zip(
-        config.areas, await asyncio.gather(*tasks, return_exceptions=True)
-    ):
-        print(f"Processed {area.name}")
-        if isinstance(info, BaseException):
-            raise info
-
-        collected[area.name] = info
+    results = await _pretty_gather(
+        *(collect(config, area) for area in config.areas)
+    )
+    collected = {
+        area.name: info
+        for area, info in zip(config.areas, results)
+    }
 
     all_areas = set(collected)
-    all_unused_store_paths = set()
+    all_unused_store_paths: set[str] = set()
     for c in collected.values():
         all_unused_store_paths.update(c.unused_store_paths)
 
-    print("Unused store paths:")
+
+    table = Table(title="Unused store paths")
+    table.add_column("Path", no_wrap=True)
+    table.add_column("Area(s)")
+
     for path in sorted(all_unused_store_paths):
         which = {k for k, v in collected.items() if path in v.unused_store_paths}
-        which_str = "" if which == all_areas else ", ".join(which)
-        print(f"{path:<120} {which_str}")
+        which_str = "All" if which == all_areas else ", ".join(which)
+
+        table.add_row(path, which_str)
+
+    console.print(table)
 
 
 def do_check(config: Config) -> None:
