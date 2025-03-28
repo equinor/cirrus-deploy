03625db334b4e370ae6b2593042e7b86edbb7494
Lars Petter Hauge <larsenhauge@gmail.com>
2025-03-28 15:08:28 +0100

    Install our own python env to be used in runscript

--- src/deploy/scripts/build_runcirrus.sh
+++ src/deploy/scripts/build_runcirrus.sh
@@ -3,4 +3,8 @@ set -ex
 
 : "${out:?}"
 
+python3.11 -m venv $out
+
 install -m 755 -D $(dirname $0)/runcirrus.py $out/bin/runcirrus
+
+sed -i "s:/usr/bin/python3.11:${out}/bin/python:g" $out/bin/runcirrus
