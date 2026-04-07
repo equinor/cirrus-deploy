afa72df28398feec37089ea155a645154a4a88df
Zohar Malamant <ZOM@equinor.com>
2026-04-07 16:17:11 +0200

    Add mkdocs website

---
+++ docs/index.md
@@ -0,0 +1,17 @@
+# Karsk
+
+--8<-- "README.md:intro"
+
+## Example
+
+The following is a simple example, showing off the configuration for building a hello-world application.
+
+```dockerfile
+# Containerfile
+--8<-- "examples/hello_world/Containerfile"
+```
+
+```yaml
+# config.yaml
+--8<-- "examples/hello_world/config.yaml"
+```
--- src/karsk/config.py
+++ src/karsk/config.py
@@ -18,53 +18,26 @@ def get_default_output_path() -> Path:
     return Path("output")
 
 
-class GitConfig(BaseModel):
-    type: Literal["git"]
-    url: str
-    ref: str
-    ssh_key_path: Annotated[Path | None, Field(exclude=True)] = None
-
-
-class FileConfig(BaseModel):
-    type: Literal["file"]
-    path: Path
-    fullpath: Annotated[Path | None, Field(exclude=True)] = None
-
-    @model_validator(mode="before")
-    @classmethod
-    def _resolve_paths(
-        cls, data: dict[str, Any], info: pydantic.ValidationInfo
-    ) -> dict[str, Any]:
-        cwd = Path((info.context or {}).get("cwd", "."))
-        data["fullpath"] = cwd / data["path"]
-        return data
-
-
-class BuildConfig(BaseModel):
-    name: str
-    version: str
-    src: GitConfig | FileConfig | None = Field(None, discriminator="type")
-    depends: list[str] = Field(default_factory=list)
-    build_image: FilePath | None = None
-
-    build: str
-
-
-class AreaConfig(BaseModel):
-    name: str
-    host: str
-
-
 class Config(BaseModel):
+    """Main configuration model"""
+
     model_config = ConfigDict(alias_generator=(lambda x: x.replace("_", "-")))
 
-    main_package: str
-    entrypoint: Path
-    build_image: FilePath
+    main_package: str = Field(description="Name of primary package (eg: 'hello')")
+    entrypoint: Path = Field(
+        description="Relative path to main executable to wrap in the run script (eg: 'bin/hello')"
+    )
+    build_image: FilePath = Field(
+        description="Path to containerfile to use for building (eg: './Containerfile')"
+    )
 
-    packages: list[BuildConfig]
-    areas: list[AreaConfig] = Field(default_factory=list)
-    links: dict[str, str] = Field(default_factory=dict)
+    packages: list[PackageConfig] = Field(description="List of packages to build")
+    areas: list[AreaConfig] = Field(
+        default_factory=list, description="Areas to sync build artifacts to"
+    )
+    links: dict[str, str] = Field(
+        default_factory=dict, description="Symbolic links setup"
+    )
 
     @field_validator("entrypoint")
     @classmethod
@@ -78,6 +51,62 @@ class Config(BaseModel):
     def _resolve_paths(cls, value: str, info: pydantic.ValidationInfo) -> Path:
         cwd = Path((info.context or {}).get("cwd", "."))
         return cwd / value
+
+
+class PackageConfig(BaseModel):
+    """The description of a package"""
+
+    name: str = Field(description="Package name")
+    version: str = Field(description="Package version")
+    src: GitConfig | FileConfig | None = Field(
+        None, discriminator="type", description="Source setup"
+    )
+    depends: list[str] = Field(default_factory=list, description="List of dependencies")
+
+    build: str = Field(
+        description="Build script, to be executed as a bash script inside of a container"
+    )
+
+
+class GitConfig(BaseModel):
+    """Sets up a git-based source"""
+
+    type: Literal["git"]
+    url: str = Field(description="URL to the git repo")
+    ref: str = Field(description="Commit reference (often a commit hash)")
+    ssh_key_path: Annotated[
+        Path | None,
+        Field(
+            exclude=True,
+            description="Path to SSH key for cloning non-public repositories",
+        ),
+    ] = None
+
+
+class FileConfig(BaseModel):
+    """Sets up an individual local file as source"""
+
+    type: Literal["file"]
+    path: Path = Field(description="Path to file, relative to config file")
+    fullpath: Annotated[
+        Path | None, Field(exclude=True, description="Absolute path to file (internal)")
+    ] = None
+
+    @model_validator(mode="before")
+    @classmethod
+    def _resolve_paths(
+        cls, data: dict[str, Any], info: pydantic.ValidationInfo
+    ) -> dict[str, Any]:
+        cwd = Path((info.context or {}).get("cwd", "."))
+        data["fullpath"] = cwd / data["path"]
+        return data
+
+
+class AreaConfig(BaseModel):
+    """TODO: AreaConfig"""
+
+    name: str = Field(description="Display name")
+    host: str = Field(description="Hostname or IP-address")
 
 
 def load_config(path: Path) -> Config:
