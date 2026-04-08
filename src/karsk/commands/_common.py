4668f24dfb64f49865fd701578b7584346e46621
Lars Petter Hauge <larsenhauge@gmail.com>
2026-04-08 16:46:25 +0200

    Add engine selection option

--- src/karsk/commands/_common.py
+++ src/karsk/commands/_common.py
@@ -6,4 +6,8 @@ import click
 argument_config_file = click.argument("config-file", type=Path)
 option_prefix = click.option("--prefix", default="./output/prefix", type=Path)
 option_output = click.option("--output", default="./output", type=Path)
-option_engine = click.option("--engine")
+option_engine = click.option(
+    "--engine",
+    type=click.Choice(["docker", "podman", "native"]),
+    default=None,
+)
--- src/karsk/commands/build.py
+++ src/karsk/commands/build.py
@@ -5,14 +5,25 @@ from pathlib import Path
 import click
 
 from karsk.builder import build_all
-from karsk.commands._common import argument_config_file, option_output, option_prefix
+from karsk.commands._common import (
+    argument_config_file,
+    option_engine,
+    option_output,
+    option_prefix,
+)
 from karsk.context import Context
+from karsk.engine import EngineName
 
 
 @click.command("build", help="Build Cirrus and dependencies")
 @argument_config_file
 @option_prefix
 @option_output
-def subcommand_build(config_file: Path, prefix: Path, output: Path) -> None:
-    context = Context.from_config_file(config_file, prefix=prefix, output=output)
+@option_engine
+def subcommand_build(
+    config_file: Path, prefix: Path, output: Path, engine: EngineName | None
+) -> None:
+    context = Context.from_config_file(
+        config_file, prefix=prefix, output=output, engine=engine
+    )
     build_all(context)
